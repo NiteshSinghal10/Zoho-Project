1da284c8e090deb985e46de3fcd058bc5b2dcc32
Nitesh <nitesh2@relinns.com>
2024-06-29 10:00:29 +0530

    update code

---
+++ dist/src/utils/response-messages/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RESPONSE_MESSAGES = void 0;
+exports.RESPONSE_MESSAGES = {
+    timesheet_add: 'Timesheet added successfully',
+};
