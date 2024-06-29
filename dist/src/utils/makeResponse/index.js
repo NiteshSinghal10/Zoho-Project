41482c0e8dbd719493ee463a3791999d33a52442
Nitesh <nitesh2@relinns.com>
2024-06-29 10:02:15 +0530

    update code

--- dist/src/utils/makeResponse/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.makeResponse = void 0;
-const makeResponse = (res, status, success, message, data = {}, meta = {}) => {
-    return res.status(status).json({ success, data: data, message: message, meta: meta });
-};
-exports.makeResponse = makeResponse;
--- dist/src/utils/response-messages/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RESPONSE_MESSAGES = void 0;
-exports.RESPONSE_MESSAGES = {
-    timesheet_add: 'Timesheet added successfully',
-};
