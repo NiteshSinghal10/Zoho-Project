bec6c24957e4ac4656a0c0ce775b5ef82c93b077
Nitesh <nitesh2@relinns.com>
2024-06-30 23:17:10 +0530

    add regex

--- src/utils/makeResponse/index.js
+++ src/utils/makeResponse/index.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.makeResponse = void 0;
 const makeResponse = (res, status, success, message, data = {}, meta = {}) => {
-    return res.status(status).json({ success, data: data, message: message, meta: meta });
+    return res.status(status).json({ success, data: data, Message: message, meta: meta });
 };
 exports.makeResponse = makeResponse;
